3d61a7e29e8fd027d790d1006e8faa359825f7cf
juanperez238421-cpu <juanperez238421@gmail.com>
2026-09-08 10:52:58 -0500

    fix: load medium-desktop Trazo containment styles

---
+++ trace-workbench-responsive.js
@@ -0,0 +1,7 @@
+const href = "./trace-workbench-responsive.css";
+if (!document.querySelector(`link[href="${href}"]`)) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = href;
+  document.head.appendChild(link);
+}
